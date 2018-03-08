7cc2ce6f9f068ec76f116ca36077c9ae4707531f
lucaspcram <lucaspcram@gmail.com>
2018-03-08 10:14:16 -0800

    Modified travis and test suite for unified version.

--- runtests.sh
+++ runtests.sh
@@ -1,20 +1,39 @@
-#!/bin/bash
+#!/bin/sh
 
 dashes=-------------------------------------------------------
+currentos=`uname`
+passed_msg="SUCCESS. All tests passed."
+failed_msg="ERROR. Test suite failed. See above output for details."
 
-if [[ ! -f ./bin/bugworld_test ]]
+if [ ! -x ./bin/bugworld_test ]
 then
     echo "Test suite not found."
     exit 1
 fi
 
-if valgrind --tool=memcheck --leak-check=full --show-leak-kinds=all --error-exitcode=1 ./bin/bugworld_test;
-then
-    echo $dashes
-    echo "ALL TESTS PASSED. VALGRIND OK."
-    exit 0
-else
-    echo $dashes
-    echo "SOME TESTS FAILED."
-    exit 1
+if [ "$currentos" == "Darwin" ]; then
+    if ./bin/bugworld_test;
+    then
+        echo $dashes
+        echo "$passed_msg"
+        exit 0
+    else
+        echo $dashes
+        echo "$failed_msg"
+        exit 1
+    fi
 fi
+
+if [ "$currentos" == "Linux" ]; then
+    if valgrind --tool=memcheck --leak-check=full --show-leak-kinds=all --error-exitcode=1 ./bin/bugworld_test;
+    then
+        echo $dashes
+        echo "$passed_msg"
+        exit 0
+    else
+        echo $dashes
+        echo "$failed_msg"
+        exit 1
+    fi
+fi
+
